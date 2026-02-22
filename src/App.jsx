b48874ab74fdc050a42a67b0338858fd09fbc001
aryankaflegithub <kaflearyan11@gmail.com>
2026-02-22 21:46:19 +0545

    Update: added scroll control and slug

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import NavbarComponent from './components/NavbarComponent';
 import FooterComponent from './components/FooterComponent';
 import HomePage from './pages/HomePage';
+import ProjectDetailPage from './pages/ProjectDetailPage';
+import useScrollToTop       from './hooks/useScrollToTop';
+
+
+const ScrollToTop = () => { useScrollToTop(); return null; };
 
 function App() {
+  const { pathname } = useLocation();
+  const isProjectPage = pathname.startsWith('/project/');
+
   return (
     <ThemeProvider>
-      <NavbarComponent />
-      <HomePage />
-      <FooterComponent />
-    </ThemeProvider>
+    <ScrollToTop />
+    {!isProjectPage && <NavbarComponent />}
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/project/:slug" element={<ProjectDetailPage />} />
+      <Route path="*" element={<HomePage />} />
+    </Routes>
+    {!isProjectPage && <FooterComponent />}
+  </ThemeProvider>
   );
 }
 
